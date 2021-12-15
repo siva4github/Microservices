8e12caa37f3882eef160c61be1b26291f7c475a3
Siva Sankar Alamalakala <leo.shiv@gmail.com>
2021-12-15 22:34:21 +0530

    Updated CommandService with ops

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -7,9 +11,13 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
-            
+            _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpPost]
@@ -18,5 +26,16 @@ namespace CommandsService.Controllers
             Console.WriteLine("--> Inbound POST # Command service");
             return Ok("Inbound test of from Platforms Controller");
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine($"--> Getting platforms from CommandService");    
+            var platforms = _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+
+
     }
 }
